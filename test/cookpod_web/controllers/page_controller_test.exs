3a7b499f7e0a131f2e1eb51fafc96371967fff2a
Pserg <pserg183@gmail.com>
2020-04-06 17:37:49 +0700

    fix test

--- test/cookpod_web/controllers/page_controller_test.exs
+++ test/cookpod_web/controllers/page_controller_test.exs
@@ -2,7 +2,7 @@ defmodule CookpodWeb.PageControllerTest do
   use CookpodWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    conn = get(conn, "/ru")
+    assert html_response(conn, 200) =~ "Добро пожаловать в Phoenix!"
   end
 end
